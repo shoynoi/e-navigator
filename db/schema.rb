07684ddf56c0ac649efbf9c66af237b0617d2f67
吉野輝 <shoynoi.jp@gmail.com>
2018-10-07 01:09:52 +0900

    rails g model Interview schedule:datetime note:text status:integer user:references

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181004194327) do
+ActiveRecord::Schema.define(version: 20181006154128) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "interviews", force: :cascade do |t|
+    t.datetime "schedule", null: false
+    t.text "note"
+    t.integer "status", default: 0
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["status"], name: "index_interviews_on_status"
+    t.index ["user_id", "schedule"], name: "index_interviews_on_user_id_and_schedule"
+    t.index ["user_id"], name: "index_interviews_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -31,4 +43,5 @@ ActiveRecord::Schema.define(version: 20181004194327) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "interviews", "users"
 end
