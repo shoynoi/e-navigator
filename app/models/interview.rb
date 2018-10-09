72c3c8ad93b66ce59b606235d3c1c97937042686
吉野輝 <shoynoi.jp@gmail.com>
2018-10-09 18:58:16 +0900

    過去の日付で登録するとエラーを表示

--- app/models/interview.rb
+++ app/models/interview.rb
@@ -1,4 +1,12 @@
 class Interview < ApplicationRecord
   enum status: { pending: 0, approved: 1, rejected: 2}
   belongs_to :user
+  validates :schedule, :status, :user_id, presence: true
+  validate :schedule_date_cannot_be_in_the_past
+
+  def schedule_date_cannot_be_in_the_past
+    if schedule.present? && schedule < DateTime.now
+      errors.add(:schedule, "can't be in the past")
+    end
+  end
 end
