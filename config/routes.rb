e3a26ee6ef9eb2ab9a90c12dc94fc9695bbdb96b
吉野輝 <shoynoi.jp@gmail.com>
2018-10-11 20:23:25 +0900

    users/registrations#editが単体のリソースを示すように修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  devise_for :users, controllers: { registrations: "users/registrations" }
+  devise_for :users, controllers: { registrations: "users/registrations" }, path_names: { edit: ":id/edit" }
   devise_scope :user do
     root :to => "devise/sessions#new"
   end
