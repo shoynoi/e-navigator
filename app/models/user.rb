efd858ab98dbbfd2cee640160785bf9257254716
吉野輝 <shoynoi.jp@gmail.com>
2018-10-12 01:19:13 +0900

    birthdayに対して未来の日付を登録できないようにvalidationを設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,9 +4,17 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   enum gender: { man: 0, woman: 1 }
+  validate :birthday_cannot_be_in_the_future
 
   def age
     date_format = "%Y%m%d"
     (Date.today.strftime(date_format).to_i - birthday.strftime(date_format).to_i) / 10000
   end
+
+  private
+    def birthday_cannot_be_in_the_future
+      if birthday.present? && birthday.future?
+        errors.add(:birthday, ": 未来の日付は使用できません。")
+      end
+    end
 end
