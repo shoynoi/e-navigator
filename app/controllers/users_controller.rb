536225b2330ee9e54dc5828eeb98630fe67b5326
吉野輝 <shoynoi.jp@gmail.com>
2018-10-12 10:22:44 +0900

    Revert "ログイン状態に応じてルートページを変更"
    This reverts commit 1227633

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  before_action :authenticate_user!, only: :index
   def index
     @users = User.where.not(id: current_user.id)
   end
--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   devise_for :users, controllers: { registrations: "users/registrations" }, path_names: { edit: ":id/edit" }
   devise_scope :user do
-    authenticated do
-      root :to => "users#index"
-    end
     root :to => "devise/sessions#new"
   end
+  resources :users, only: [:index]
 end
