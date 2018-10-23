4be8cdabc2995633a39c5b478f8be32129c571da
吉野輝 <shoynoi.jp@gmail.com>
2018-10-24 01:29:59 +0900

    refactor validation

--- app/models/interview.rb
+++ app/models/interview.rb
@@ -1,12 +1,6 @@
 class Interview < ApplicationRecord
   enum status: { pending: 0, approved: 1, rejected: 2}
   belongs_to :user
-  validates :schedule, :status, :user_id, presence: true
-  validate :schedule_date_cannot_be_in_the_past
-
-  def schedule_date_cannot_be_in_the_past
-    if schedule.present? && schedule < DateTime.now
-      errors.add(:schedule, "can't be in the past")
-    end
-  end
+  validates :status, :user_id, presence: true
+  validates :schedule, presence: true, schedule: { allow_blank: true }
 end
